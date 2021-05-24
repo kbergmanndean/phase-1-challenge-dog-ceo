1a95d1e4713a2a83b98fa941a29e17c4ea39503a
Kaia <kbergmanndean@gmail.com>
2021-05-24 17:42:15 -0400

    commit tree b216e2b90f6c657d3101fb7579638b1c1182a8db

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", ()=> {
+fetch("https://dog.ceo/api/breeds/image/random/4" )
+.then(response=>response.json())
+.then(data=> renderEach(data))
+
+const renderEach=function(dataObject) {
+    let dogContainer=document.querySelector("#dog-image-container")
+    let dogArray= dataObject.message
+    dogArray.forEach((element)=> {
+        let img=document.createElement("img")
+        img.src=element
+        dogContainer.append(img)
+
+    })
+
+}
+
+fetch("https://dog.ceo/api/breeds/list/all")
+.then(response=> response.json())
+.then(data=>(renderBreeds(data)))
+const renderBreeds= function(dataObject) {
+    const breedObject=dataObject.message
+    const breedList=document.querySelector("#dog-breeds")
+    for (const key in breedObject) {
+        let item=document.createElement("li")
+        item.textContent=key;
+        breedList.append(item)
+        item.addEventListener("click", ()=> {
+            item.style.color="purple"
+        })
+    }   
+}
+ 
+ 
+    
+
+
+
+
+})
